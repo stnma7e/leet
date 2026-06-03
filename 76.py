f407afe2449690667ed36542c352b9456b3b29f0
Sam Delmerico <svdelmerico@gmail.com>
2026-06-03 12:36:26 -0700

    76: additional solution

--- 76.py
+++ 76.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         start = 0
@@ -29,3 +31,43 @@ class Solution:
                 end += 1
                 if end <= len(s):
                     seen[s[end - 1]] += 1
+
+
+"""
+Using Counter instead of rolling our own dict
+"""
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        start = 0
+        end = 1
+        best = ""
+        charcounts = {c: 0 for c in t}
+        for c in t:
+            charcounts[c] += 1
+        needed = Counter(t)
+        if s[start] in charcounts:
+            needed.subtract([s[start]])
+        while True:
+            # print(start, end, s[start:end], needed)
+            if start >= len(s):
+                return best
+            if s[start] not in charcounts:
+                start += 1
+                continue
+            if (+needed).total() == 0:
+                if best == "" or end - start < len(best):
+                    best = s[start:end]
+                if s[start] in charcounts:
+                    needed.update([s[start]])
+                start += 1
+            else:
+                if end != len(s):
+                    end += 1
+                    newchar = s[end - 1]
+                    if newchar in charcounts:
+                        needed.subtract([s[end - 1]])
+                else:
+                    if s[start] in charcounts:
+                        needed.update([s[start]])
+                    start += 1
+
