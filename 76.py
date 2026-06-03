d6503b634940cdd061cf31c0c3ad1e2194e603ae
Sam Delmerico <svdelmerico@gmail.com>
2026-06-03 12:16:15 -0700

    76: accepted

--- 76.py
+++ 76.py
@@ -6,25 +6,26 @@ class Solution:
         charcounts = {c: 0 for c in t}
         for c in t:
             charcounts[c] += 1
+        seen = defaultdict(int)
+        seen[s[start]] += 1
         while True:
-            # print(start, end, s[start:end])
+            # print(start, end, s[start:end], seen)
             if end > len(s) or start >= len(s):
                 return best
-            if s[start] not in t:
+            if s[start] not in charcounts:
+                seen[s[start]] -= 1
                 start += 1
                 continue
-            seen = defaultdict(int)
-            for c in s[start:end]:
-                if c in charcounts:
-                    seen[c] += 1
             allSeen = all([
                 count <= seen[c]
                 for c, count in charcounts.items()
             ])
-            # print(seen)
             if allSeen:
                 if best == "" or end - start < len(best):
                     best = s[start:end]
+                seen[s[start]] -= 1
                 start += 1
             else:
                 end += 1
+                if end <= len(s):
+                    seen[s[end - 1]] += 1
